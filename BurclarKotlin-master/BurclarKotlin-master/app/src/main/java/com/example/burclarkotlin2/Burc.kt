4d748d2edf5c660ab410986ec6978cb97e252468
aleynacoskun <83537794+aleynacoskun@users.noreply.github.com>
2023-06-12 16:27:46 +0300

    burc uygulaması
    Android kotlin burç uygulaması

---
+++ BurclarKotlin-master/BurclarKotlin-master/app/src/main/java/com/example/burclarkotlin2/Burc.kt
@@ -0,0 +1,5 @@
+package com.example.burclarkotlin2
+
+import java.io.Serializable
+
+data class Burc(var burcAdi:String , var burcTarihi:String , var burcSembol:Int, var burcBuyukResim:Int , var burcGenelOzellikleri:String):Serializable{}
